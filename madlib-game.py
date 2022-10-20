50456a7bee52709e702b23206fd54645d5788ce8
Eoin Reid <40269943+EoinReid@users.noreply.github.com>
2022-10-20 10:53:15 +0100

    adds madlib game

---
+++ madlib-game.py
@@ -0,0 +1,8 @@
+# Madlibs game
+colour = input("Enter a colour: ")
+plural_noun = input("Enter a plural noun: ")
+favourite_animal = input("Enter your favourite animal: ")
+
+print("Roses are "+colour)
+print(plural_noun+" are blue")
+print("My favourite type of animal is " +favourite_animal)
